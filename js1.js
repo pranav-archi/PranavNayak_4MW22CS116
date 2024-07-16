80d69e3664c08cffefdf19977ff98a4c820742ba
pranav-archi <158431652+pranav-archi@users.noreply.github.com>
2024-07-16 21:30:07 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,15 @@
+function findMax(arr){
+    if(arr.length===0){
+        return null
+    }
+    let maxValue=arr[0]
+    for(let item=0;item<arr.length;item++){
+        if (arr[item]>maxValue) {
+            maxValue=arr[item]
+        }
+    }
+    return maxValue
+}
+let numbers=[1,4,5,7,8,10]
+let result = findMax(numbers);
+console.log(result);
